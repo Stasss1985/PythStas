261eaf0afb950a76d288d4de4b7728b957a0770d
Stasss1985 <Stasss_1985@mail.ru>
2024-11-05 18:48:55 +0300

    refacnoring

--- leadTests/lead_karman.py
+++ leadTests/lead_karman.py
@@ -1,67 +1,77 @@
 import time
 from selenium.webdriver.common.by import By
 from selenium import webdriver
-from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+
 
 browser = webdriver.Chrome()
 browser.get('https://erp-test.karman24.ru/')
-time.sleep(3)
-
-imputMail = browser.find_element(By.XPATH, '//*[@id="email"]')
+browser.maximize_window()
+WebDriverWait(browser, 10).until(
+    EC.element_to_be_clickable((By.CLASS_NAME, 'btn.btn-primary'))
+)
+imputMail = browser.find_element(By.ID, 'email')
 imputMail.send_keys('krivko.su@codeagency.ru')
 
-imputPassword = browser.find_element(By.XPATH, '//*[@id="password"]')
+imputPassword = browser.find_element(By.ID, 'password')
 imputPassword.send_keys('DLNKsfd3214$%23')
 
+imputButton = browser.find_element(By.CLASS_NAME, 'btn.btn-primary')
+imputButton.click()
+# Ожидание до 5 секунд, пока кнопка "Сменить" станет доступной.
+WebDriverWait(browser, 10).until(
+    EC.text_to_be_present_in_element((By.TAG_NAME, 'body'), 'Сменить'))
 
-try:
-    # Ожидание до 5 секунд, пока элемент станет доступен
-    imputButton = WebDriverWait(browser, 5).until(
-        EC.element_to_be_clickable((By.CLASS_NAME, 'btn.btn-primary'))
-    )
-    imputButton.click()  # Вызов метода click()
-except Exception as e:
-    print(f"Ошибка: {e}")
-finally:
-    time.sleep(3)  # Ждем 3 секунд, чтобы увидеть результат
+changeButton = browser.find_element(
+    By.CLASS_NAME, 'p-button.p-component.mt-2.p-button-warning')
+changeButton.click()
 
-try:
-    # Ожидание до 5 секунд, пока кнопка "Сменить" станет доступной.
-    changeButton = WebDriverWait(browser, 5).until(
-        EC.element_to_be_clickable(
-            (By.CLASS_NAME, 'p-button.p-component.mt-2.p-button-warning'))
-    )
-    changeButton.click()  # Вызов метода click()
-    time.sleep(5)
-    # Ожидание до 5 сек, пока Кнопка офис "ООО 1 мая" станет доступной.
-    imput_Office = WebDriverWait(browser, 5).until(
-        EC.element_to_be_clickable(
-            (By.TAG_NAME, 'input')))
-    imput_Office.send_keys('Краснодар-1-го Мая ЛОМБАРД (Карман)')
+input_fild = browser.find_element(By.CLASS_NAME, 'p-tree-filter-container')
+input_fild.click()
 
-    imput_Office.click()  # Вызов метода click()
+input_fild2 = WebDriverWait(browser, 10).until(
+    EC.element_to_be_clickable((By.CSS_SELECTOR, '[aria-label="Краснодар-1-го Мая ЛОМБАРД (Карман)"]')))
+input_fild2.click()
+# input_fild2 = browser.find_element(
+#    By.CSS_SELECTOR, '[aria-label="Краснодар-1-го Мая ЛОМБАРД (Карман)"]')
 
-except Exception as e:
-    print(f"Ошибка: {e}")
-finally:
-    time.sleep(5)  # Ждем 3 секунд, чтобы увидеть результат
+# input_fild2 = browser.find_element(
+#    By.XPATH, '/html/body/div[2]/div/div[2]/div/div[2]/ul/li[10]/ul/li[10]/div/span[2]')
+# time.sleep(5)
+# time.sleep(10)
+# WebDriverWait(browser, 10).until(
+#    EC.text_to_be_present_in_element((By.TAG_NAME, 'body'), 'Лиды'))
+# lead_href = browser.find_element(By.CSS_SELECTOR, '[href="/lead"]')
+# lead_href.click()
+# href = "/lead/create"
 
-
-try:
-    lead_href = WebDriverWait(browser, 5).until(
-        EC.element_to_be_clickable(
-            (By.XPATH, '//*[@id="leftside-menu-container"]/div[1]/div[2]/div/div/div/ul/li[2]/a'))
-    )
-    lead_href.click()
-except Exception as e:
-    print(f"Ошибка {e}")
-finally:
-    time.sleep(5)
 
 # lead_href = browser.find_element(
 #    By.XPATH, '//*[@id="leftside-menu-container"]/div[1]/div[2]/div/div/div/ul/li[2]/a')
 # lead_href.click()
 # time.sleep(10)
 
+lead_href = WebDriverWait(browser, 10).until(
+    EC.element_to_be_clickable((By.CSS_SELECTOR, '[href="/lead"]')))
+lead_href.click()
+
+lead_create_grin = WebDriverWait(browser, 10).until(
+    EC.element_to_be_clickable((By.CSS_SELECTOR, '[href = "/lead/create"]')))
+lead_create_grin.click()
+
+counteragent_choose = WebDriverWait(browser, 10).until(
+    EC.element_to_be_clickable((By.ID, 'counteragentId')))
+counteragent_choose.click()
+
+counteragent_choose2 = browser.find_element(
+    By.CSS_SELECTOR, '[aria-controls="counteragentId_list"]')
+counteragent_choose2.send_keys('Кривко С.Ю.')
+
+counteragent_choose2 = WebDriverWait(browser, 10).until(
+    EC.element_to_be_clickable((By.CSS_SELECTOR, '[aria-label="Кривко С.Ю."]')))
+counteragent_choose2.click()
+
+
+time.sleep(7)
 browser.quit()  # Закрываем браузер
